79b3661bda08d23f062ad81f87d0b127c7260c30
developer-nieem <naimhossenwork@gmail.com>
2023-05-02 16:04:25 +0600

    made chef api path and show one chef all information another path

--- index.js
+++ index.js
@@ -4,8 +4,21 @@ const cors = require('cors')
 const port = 3000
 
 app.use(cors())
+
+const chefData =  require('./public/chef.json');
+
 app.get('/', (req, res) => {
   res.send('Welcome to assignment 10 server')
+})
+
+app.get('/chef', (req, res)=>{
+    res.send(chefData)
+})
+
+app.get('/chef/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedChef = chefData.find(c=> c.id == id);
+    res.send(selectedChef)
 })
 
 app.listen(port, () => {
